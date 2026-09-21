2039079ce955862b013bb76a6df30de27cad0daf
mquinteror1000 <martinsilloelfilosofo@gmail.com>
2026-09-21 10:07:10 -0600

    sources

--- debian/02_install_docker_ce.sh
+++ debian/02_install_docker_ce.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-instalar los docker Comunity Edition
+#instalar los docker Comunity Edition
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
